a4e873caa2784f1a2a407a30ceab94bdf4677d7a
Dasha <d.bieliava@fashionette.de>
2017-11-26 19:02:34 +0100

    playing around with Point class

--- sandbox/src/main/java/pdt/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pdt/sandbox/MyFirstProgram.java
@@ -1,31 +1,14 @@
 package pdt.sandbox;
 
 import java.util.Scanner;
-
 public class MyFirstProgram {
-    static Point p = new Point();
 
-    public static void showValues() {
-     //   Point p = new Point();
-
-        System.out.println("x1 = " + p.x1);
-        System.out.println("x2 = " + p.x2);
-        System.out.println("y1 = " + p.y1);
-        System.out.println("y2 = " + p.y2);
-
-    }
-
-    public static void distance() {
-        double d;
-
-        d =Math.sqrt(Math.pow(p.x2-p.x1, 2) + Math.pow(p.y2-p.y1, 2));
-         System.out.println("d = "+ d);
-       // return d;
-    }
 
     public static void main(String[] args) {
-
+        Point p = new Point();
+        p.setPoint();
+        p.printPoint();
+        p.distanceBetweenPoints();
     }
-
 
 }
--- sandbox/src/main/java/pdt/sandbox/Point.java
+++ sandbox/src/main/java/pdt/sandbox/Point.java
@@ -2,8 +2,6 @@ package pdt.sandbox;
 
 import java.util.Scanner;
 
-import static pdt.sandbox.MyFirstProgram.distance;
-import static pdt.sandbox.MyFirstProgram.showValues;
 
 public class Point {
     int x1;
@@ -11,20 +9,37 @@ public class Point {
     int y1;
     int y2;
 
-    Point() {
+  public  Point() {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+
+    }
+     void setPoint(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Type x1, x2, y1, y2 value for calculation...");
-        this.x1 = sc.nextInt();
-        this.x2 = sc.nextInt();
-        this.y1 = sc.nextInt();
-        this.y2 = sc.nextInt();
-
+        x1 = sc.nextInt();
+        x2 = sc.nextInt();
+        y1 = sc.nextInt();
+        y2 = sc.nextInt();
     }
 
+    void printPoint(){
+        System.out.println("x1 = " + x1);
+        System.out.println("x2 = " + x2);
+        System.out.println("y1 = " + y1);
+        System.out.println("y2 = " + y2);
+    }
 
-    public static void main(String[] args) {
-        showValues();
-        distance();
+     void distanceBetweenPoints(){
+        double d;
+        //Point p = new Point();
+        d =Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+        System.out.println("d = "+ d);
+    }
+
+    public static void main(String[] args){
+
     }
 }
-
