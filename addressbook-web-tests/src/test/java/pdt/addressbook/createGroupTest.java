3535a790c8e930e0ee76f0163446f9d97d4b6ec5
Dasha <dasharb24@gmail.com>
2017-12-07 16:30:09 +0100

    GroupData Parameter object was created

--- addressbook-web-tests/src/test/java/pdt/addressbook/createGroupTest.java
+++ addressbook-web-tests/src/test/java/pdt/addressbook/createGroupTest.java
@@ -33,7 +33,7 @@ public class createGroupTest {
     public void testCreateGroup() {
 
         initGroup();
-        fillGroupForm("test", "test1", "test2");
+        fillGroupForm(new GroupData("test", "test1", "test2"));
         submitGroupCreation();
         returnToGroupPage();
     }
@@ -46,16 +46,16 @@ public class createGroupTest {
         wd.findElement(By.name("submit")).click();
     }
 
-    private void fillGroupForm(String groupName, String groupHeader, String groupFooter) {
+    private void fillGroupForm(GroupData groupData) {
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(groupName);
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getGroupName());
         wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(groupHeader);
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getGroupHeader());
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(groupFooter);
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getGroupFooter());
     }
 
     private void initGroup() {
