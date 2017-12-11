c732e24e4a09e49b8e9b55ecbd7ff9ba4372f866
Dasha <dasharb24@gmail.com>
2017-12-11 10:55:31 +0100

    refactoring class names

---
+++ addressbook-web-tests/src/test/java/pdt/addressbook/tests/ContactCreationTests.java
@@ -0,0 +1,17 @@
+package pdt.addressbook.tests;
+import org.testng.annotations.Test;
+import pdt.addressbook.models.ContactAddress;
+import pdt.addressbook.models.ContactNameSurname;
+
+
+public class ContactCreationTests extends TestBase{
+
+    @Test
+    public void createContactTest() {
+        app.getContactHelper().initCreatContact();
+        app.getContactHelper().fillContactNameSurnameSalutation(new ContactNameSurname("Dasha", "Test", "nickname", "MS"));
+        app.getContactHelper().fillContactPersonalData(new ContactAddress("someCompany", "Grafenberger Allee 222", "1111111", "dasha@gmail.com"));
+        app.getContactHelper().modifyContact();
+    }
+
+}
