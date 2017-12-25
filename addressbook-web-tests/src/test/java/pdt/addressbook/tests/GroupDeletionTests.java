fb00c991deb1972f51e6cae9b8f398d844744b0d
Dasha <d.bieliava@fashionette.de>
2017-12-25 09:52:27 +0100

    check if group doesnt exist

--- addressbook-web-tests/src/test/java/pdt/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/pdt/addressbook/tests/GroupDeletionTests.java
@@ -3,7 +3,13 @@ package pdt.addressbook.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pdt.addressbook.models.GroupData;
+
+import java.util.UUID;
+
+import static org.openqa.selenium.By.name;
 
 
 public class GroupDeletionTests extends TestBase {
@@ -18,6 +24,24 @@ public class GroupDeletionTests extends TestBase {
         groupHelper.returnToGroupPage();
         webDriverWait.withMessage("User is not redirected to the group page");
         webDriverWait.until(ExpectedConditions.urlContains("group.php"));
+    }
+
+    @BeforeMethod
+    public void createGroupIfDoesNotExist() {
+        navigationHelper.goToGroupPage();
+        //  if (!groupHelper.isElementPresent(name("selected[]"))) {
+        if(!groupHelper.isAnyGroupExists()){
+
+            groupHelper.initGroup();
+            GroupData group = new GroupData();
+            group.group_name = String.format("Group name%s", UUID.randomUUID());
+            group.group_footer = String.format("Footer%s", UUID.randomUUID());
+            group.group_header = String.format("Header%s", UUID.randomUUID());
+
+
+            groupHelper.fillGroupForm(group);
+            groupHelper.submitGroupCreation();
+        }
     }
 
 }
