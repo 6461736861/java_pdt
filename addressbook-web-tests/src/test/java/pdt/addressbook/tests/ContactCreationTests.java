1daef8f5e02e6ebd8d099db38a6f1d2e16b7e0f5
Dasha <d.bieliava@fashionette.de>
2018-02-01 12:08:03 +0100

    lecture 6.5 Contacts

--- addressbook-web-tests/src/test/java/pdt/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/pdt/addressbook/tests/ContactCreationTests.java
@@ -4,8 +4,12 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pdt.addressbook.models.ContactData;
 import pdt.addressbook.models.Contacts;
+import pdt.addressbook.models.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -17,11 +21,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validContacts() {
+    public Iterator<Object[]> validContacts() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[]{new ContactData().withName("test name1").withaddress("someAddress1").withEmail("someemail@gmail.com")});
-        list.add(new Object[]{new ContactData().withName("test name23").withaddress("someAddress44").withEmail("someema45il@gmail.com")});
-        list.add(new Object[]{new ContactData().withName("test name234").withaddress("someAddress66").withEmail("someem765ail@gmail.com")});
+        BufferedReader reader = new BufferedReader(new FileReader("/Users/rb/java_pdt/addressbook-web-tests/src/test/resources/contacts.csv"));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[]{new ContactData().withName(split[0]).withaddress(split[1]).withEmail(split[2])});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
