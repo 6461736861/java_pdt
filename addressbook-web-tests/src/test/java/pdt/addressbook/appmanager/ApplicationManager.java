226ebb4c2dc8e7f93b0bc8acae11dd15377af85f
Dasha <d.bieliava@fashionette.de>
2018-02-04 18:31:56 +0100

    homework 6.14

--- addressbook-web-tests/src/test/java/pdt/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/pdt/addressbook/appmanager/ApplicationManager.java
@@ -3,6 +3,11 @@ package pdt.addressbook.appmanager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -15,12 +20,16 @@ public class ApplicationManager {
     protected SessionHelper sessionHelper;
     protected ContactHelper contactHelper;
     protected String browser;
+    private final Properties properties;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
         System.setProperty("webdriver.gecko.driver", "/Users/rb/java_pdt/addressbook-web-tests/geckodriver");
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -28,9 +37,8 @@ public class ApplicationManager {
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
         contactHelper = new ContactHelper(wd);
-        wd.get("http://localhost:81/addressbook/");
-      //  wd.get("http://localhost/addressbook/");
-        sessionHelper.login("admin", "secret");
+        wd.get(properties.getProperty("web.baseUrl"));
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
 
--- addressbook-web-tests/src/test/java/pdt/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pdt/addressbook/tests/TestBase.java
@@ -5,16 +5,19 @@ import org.testng.annotations.BeforeSuite;
 import pdt.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.IOException;
+
 
 /**
  * Created by rb on 12/8/17.
  */
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
-    public void setUP() {
+    public void setUP() throws IOException {
         app.init();
     }
 
